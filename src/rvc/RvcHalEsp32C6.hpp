2f10b7015d57668c981d12c029442cfe0a4276aa
Nebiyu Tadesse <nebysma@gmail.com>
2025-05-22 20:23:17 -0600

    Refactor RVC code for HAL abstraction

---
+++ src/rvc/RvcHalEsp32C6.hpp
@@ -0,0 +1,96 @@
+#pragma once
+#include "RvcHal.hpp"
+#include "driver/gpio.h"
+#include "driver/uart.h"
+#include "esp_timer.h"
+#include <cstring>
+
+/**
+ * @brief Example HAL implementation for ESP32-C6 using ESP-IDF UART driver.
+ */
+class Esp32C6RvcHal : public IRvcHal {
+public:
+  explicit Esp32C6RvcHal(uart_port_t port = UART_NUM_0,
+                         gpio_num_t tx = GPIO_NUM_21,
+                         gpio_num_t rx = GPIO_NUM_20,
+                         gpio_num_t rst = GPIO_NUM_9,
+                         gpio_num_t boot = GPIO_NUM_10)
+      : _port(port), _tx(tx), _rx(rx), _rst(rst), _boot(boot) {}
+
+  int open() override {
+    uart_config_t cfg{};
+    cfg.baud_rate = 115200;
+    cfg.data_bits = UART_DATA_8_BITS;
+    cfg.parity = UART_PARITY_DISABLE;
+    cfg.stop_bits = UART_STOP_BITS_1;
+    cfg.flow_ctrl = UART_HW_FLOWCTRL_DISABLE;
+    uart_param_config(_port, &cfg);
+    uart_set_pin(_port, _tx, _rx, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+    uart_driver_install(_port, BUF_SIZE, 0, 0, nullptr, 0);
+    gpio_set_level(_boot, 1);
+    gpio_set_level(_rst, 0);
+    ets_delay_us(10000);
+    gpio_set_level(_rst, 1);
+    _len = 0;
+    _ready = false;
+    return RVC_OK;
+  }
+
+  void close() override { uart_driver_delete(_port); }
+
+  int read(rvc_SensorEvent_t *event) override {
+    uint8_t c;
+    while (uart_read_bytes(_port, &c, 1, 0) == 1) {
+      process(c);
+    }
+    if (_ready) {
+      fillEvent(event);
+      _ready = false;
+      _len = 0;
+      return 1;
+    }
+    return 0;
+  }
+
+private:
+  static constexpr int BUF_SIZE = 256;
+  static constexpr int FRAME_LEN = 19;
+  uart_port_t _port;
+  gpio_num_t _tx, _rx, _rst, _boot;
+  uint8_t _frame[FRAME_LEN];
+  size_t _len{0};
+  bool _ready{false};
+
+  static bool checksum(const uint8_t *f) {
+    uint8_t check = 0;
+    for (int i = 2; i < FRAME_LEN - 1; ++i)
+      check += f[i];
+    return check == f[FRAME_LEN - 1];
+  }
+
+  void process(uint8_t c) {
+    if (_len == FRAME_LEN) {
+      memmove(_frame, _frame + 1, FRAME_LEN - 1);
+      _frame[FRAME_LEN - 1] = c;
+    } else {
+      _frame[_len++] = c;
+    }
+    if (_len == FRAME_LEN && _frame[0] == 0xAA && _frame[1] == 0xAA &&
+        checksum(_frame)) {
+      _ready = true;
+    }
+  }
+
+  void fillEvent(rvc_SensorEvent_t *e) {
+    e->timestamp_uS = esp_timer_get_time();
+    e->index = _frame[2];
+    e->yaw = (int16_t)((_frame[4] << 8) | _frame[3]);
+    e->pitch = (int16_t)((_frame[6] << 8) | _frame[5]);
+    e->roll = (int16_t)((_frame[8] << 8) | _frame[7]);
+    e->acc_x = (int16_t)((_frame[10] << 8) | _frame[9]);
+    e->acc_y = (int16_t)((_frame[12] << 8) | _frame[11]);
+    e->acc_z = (int16_t)((_frame[14] << 8) | _frame[13]);
+    e->mi = _frame[15];
+    e->mr = _frame[16];
+  }
+};
--- src/rvc/rvc.h
+++ src/rvc/rvc.h
@@ -2,7 +2,7 @@
  * Copyright 2020-21 CEVA, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License and 
+ * you may not use this file except in compliance with the License and
  * any applicable agreements you may have with CEVA, Inc.
  * You may obtain a copy of the License at
  *
@@ -15,75 +15,77 @@
  * limitations under the License.
  */
 
-
 #ifndef RVC_H
 #define RVC_H
 
 #include <stdint.h>
 
+#ifdef __cplusplus
+class IRvcHal;
+extern "C" {
+#endif
+
 // return status values
-#define RVC_OK                 (0)  /**< Success */
-#define RVC_ERR                (-1) /**< General Error */
-#define RVC_ERR_BAD_PARAM      (-2) /**< Bad parameter to an API call */
+#define RVC_OK (0)                  /**< Success */
+#define RVC_ERR (-1)                /**< General Error */
+#define RVC_ERR_BAD_PARAM (-2)      /**< Bad parameter to an API call */
 #define RVC_ERR_OP_IN_PROGRESS (-3) /**< Operation in progress */
-#define RVC_ERR_IO             (-4) /**< Error communicating with hub */
-#define RVC_ERR_HUB            (-5) /**< Error reported by hub */
-#define RVC_ERR_TIMEOUT        (-6) /**< Operation timed out */
+#define RVC_ERR_IO (-4)             /**< Error communicating with hub */
+#define RVC_ERR_HUB (-5)            /**< Error reported by hub */
+#define RVC_ERR_TIMEOUT (-6)        /**< Operation timed out */
 
 // mi field, Motion Intent, values:
-#define MI_UNKNOWN                       (0)
-#define MI_STATIONARY_WITHOUT_VIBRATION  (1)
-#define MI_STATIONARY_WITH_VIBRATION     (2)
-#define MI_IN_MOTION                     (3)
+#define MI_UNKNOWN (0)
+#define MI_STATIONARY_WITHOUT_VIBRATION (1)
+#define MI_STATIONARY_WITH_VIBRATION (2)
+#define MI_IN_MOTION (3)
 
-#define MR_NO_CONSTRAINT                 (0)
-#define MR_STAY_STATIONARY_REQUIRED      (1)
-#define MR_STAY_STATIONARY_OPTIONAL      (2) // Deprecated, ignore
-#define MR_NON_URGENT_STAY_STATIONARY    (3)
-#define MR_URGENT_STATIONARY             (4)
-#define MR_TIMER_STATIONARY              (5)
+#define MR_NO_CONSTRAINT (0)
+#define MR_STAY_STATIONARY_REQUIRED (1)
+#define MR_STAY_STATIONARY_OPTIONAL (2) // Deprecated, ignore
+#define MR_NON_URGENT_STAY_STATIONARY (3)
+#define MR_URGENT_STATIONARY (4)
+#define MR_TIMER_STATIONARY (5)
 
 // Use integers as references to RVC HAL instances.
 // (Maybe change to a structure later?)
 typedef int RvcHal_t;
 
-typedef struct rvc_SensorEvent_s
-{
-    uint8_t index;
-    int16_t yaw;
-    int16_t pitch;
-    int16_t roll;
-    int16_t acc_x;
-    int16_t acc_y;
-    int16_t acc_z;
-    uint8_t mi;      // Motion intent
-    uint8_t mr;      // Motion request
-    uint64_t timestamp_uS;
+typedef struct rvc_SensorEvent_s {
+  uint8_t index;
+  int16_t yaw;
+  int16_t pitch;
+  int16_t roll;
+  int16_t acc_x;
+  int16_t acc_y;
+  int16_t acc_z;
+  uint8_t mi; // Motion intent
+  uint8_t mr; // Motion request
+  uint64_t timestamp_uS;
 } rvc_SensorEvent_t;
 
-typedef struct rvc_SensorValue_s
-{
-    uint8_t index;
-    float yaw_deg;
-    float pitch_deg;
-    float roll_deg;
-    float acc_x_g;
-    float acc_y_g;
-    float acc_z_g;
-    uint8_t mi;      // Motion intent
-    uint8_t mr;      // Motion request
-    uint64_t timestamp_uS;
+typedef struct rvc_SensorValue_s {
+  uint8_t index;
+  float yaw_deg;
+  float pitch_deg;
+  float roll_deg;
+  float acc_x_g;
+  float acc_y_g;
+  float acc_z_g;
+  uint8_t mi; // Motion intent
+  uint8_t mr; // Motion request
+  uint64_t timestamp_uS;
 } rvc_SensorValue_t;
 
 typedef void rvc_Callback_t(void *cookie, rvc_SensorEvent_t *pEvent);
 
 // Public API calls used by demo_rvc.c
 
-// Initialize the RVC sensor hub module
-int rvc_init();
+// Initialize the RVC sensor hub module with a HAL implementation
+int rvc_init(IRvcHal *hal);
 
 // Application registers callback function to receive sensor events
-int rvc_setCallback(rvc_Callback_t *callback, void * cookie);
+int rvc_setCallback(rvc_Callback_t *callback, void *cookie);
 
 // Open starts the flow of sensor events from the sensor hub.
 int rvc_open();
@@ -98,5 +100,9 @@ void rvc_service();
 
 // Convert data from integers, fixed point to floating point, natural units.
 void rvc_decode(rvc_SensorValue_t *value, const rvc_SensorEvent_t *event);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
