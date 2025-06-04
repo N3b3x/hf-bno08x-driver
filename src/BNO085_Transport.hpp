c9292fc67a9a1d61c87975ed0a11f5077874b444
Nebiyu Tadesse <nebysma@gmail.com>
2025-06-04 17:33:48 -0600

    Add pin control hooks

--- src/BNO085_Transport.hpp
+++ src/BNO085_Transport.hpp
@@ -63,4 +63,28 @@ public:
    * The transport implementation must provide this in microseconds.
    */
   virtual uint32_t getTimeUs() = 0;
+
+  /**
+   * @brief Control the hardware reset (RSTN) pin.
+   *
+   * The default implementation does nothing. Platforms that connect the
+   * sensor's RSTN pin can override this to assert or release reset.
+   */
+  virtual void setReset(bool state) {}
+
+  /**
+   * @brief Control the BOOTN pin used to enter DFU mode.
+   *
+   * Driving BOOTN low during a reset places the device in the bootloader.
+   * Implementations may leave this empty if the pin is not wired.
+   */
+  virtual void setBoot(bool state) {}
+
+  /**
+   * @brief Control the WAKE pin (SPI mode only).
+   *
+   * When using SPI, pulling WAKE low brings the device out of suspend. Not
+   * all designs expose this pin, so the default does nothing.
+   */
+  virtual void setWake(bool state) {}
 };
